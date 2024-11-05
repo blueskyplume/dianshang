18c4fce7117899dd8333c5305af75a5b412c18fd
lianlanyu <2411620171@qq.com>
2024-11-05 16:52:23 +0800

    完成主题色定制

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,38 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
+    // 自动引入Elment组件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        // 配置elmentplus采用sass样式配色系统
+        ElementPlusResolver(
+          {importStyle: 'sass'}
+        )
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `@use "@/styles/element/index.scss" as *;`
+      }
     }
   }
 })
